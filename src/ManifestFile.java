ef53058bf94cf962b39c3404647c1ed0a34d6361
RobinTheSprite <mark.t.underwood@protonmail.com>
2020-12-22 17:32:10 -0900

    Document ManifestFile.java

--- src/ManifestFile.java
+++ src/ManifestFile.java
@@ -1,7 +1,7 @@
-/*
+/**
  *
  * ManifestFile.java
- * Mark Underwood
+ * @author Mark Underwood
  * https://github.com/RobinTheSprite
  * 12/21/2020
  *
@@ -12,9 +12,18 @@ package src;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Stores and converts Java manifest headers to XML snippets
+ * suitable for ant scripts.
+ */
 public class ManifestFile {
     private HashMap<String, String> headers = new HashMap<>();
 
+    /**
+     * Convert a stored header into an attribute element
+     * @param name
+     * @return String containing an Ant attribute element.
+     */
     private String headerString(String name) {
         return String.format(
             "<attribute name=\"%s\" value=\"%s\" />",
@@ -23,10 +32,21 @@ public class ManifestFile {
         );
     }
 
+    /**
+     * Add a header to the manifest file. A header must have
+     * both a name and a value.
+     * @param name
+     * @param value
+     */
     public void addHeader(String name, String value) {
         headers.put(name, value);
     }
 
+    /**
+     * Remove a header from the manifest file.
+     * @param name
+     * @return String containing the removed value.
+     */
     public String removeHeader(String name) {
         String value = headers.get(name);
 
@@ -35,6 +55,11 @@ public class ManifestFile {
         return value;
     }
 
+    /**
+     *
+     * @return An ArrayList of Strings, which contain the Ant
+     * attribute elements for the currently stored headers.
+     */
     public ArrayList<String> getHeaders() {
         ArrayList<String> headerStrings = new ArrayList<>();
         for (String name : headers.keySet()) {
